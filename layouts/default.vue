a8663d4c7b1f6913eef952268bd275c76343a60b
Joshua Galit <joshuaimalay@gmail.com>
2021-01-11 20:08:21 +0800

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="font-sans bg-gray-100 antialiased min-h-screen h-full text-gray-900">
+
+    <main>
+      <Nuxt />
+    </main>
+  </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Youtube</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    head () {
+      return {
+        title: 'Youtube'
+      }
+    }
+  }
+</script>
