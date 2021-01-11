c417493e22bcac87aee1cc737d920bd84dc351e5
Joshua Galit <joshuaimalay@gmail.com>
2021-01-11 20:18:27 +0800

    import TheNavBar vue component set as header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="font-sans bg-gray-100 antialiased min-h-screen h-full text-gray-900">
-
+    <TheNavBar />
     <main>
       <Nuxt />
     </main>
@@ -9,5 +9,8 @@
 
 <script>
   export default {
+    components: {
+      TheNavBar: () => import('@/components/TheNavBar')
+    }
   }
 </script>
