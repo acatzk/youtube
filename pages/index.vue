e712d92f8fa01cd25d4767d937b2a5b167a40c47
Joshua Galit <joshuaimalay@gmail.com>
2021-01-12 09:22:23 +0800

    add flex layout import: TheSideBar vue component

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-    <h1>Youtube</h1>
+  <div class="flex min-h-screen h-full">
+    <div class="w-56 flex-none border-r bg-gray-200">
+      <TheSideBar />
+    </div>
+    <div class="flex-1 w-full">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae excepturi nisi dolorum illum sunt, maiores animi debitis nobis quia officia vitae, provident reprehenderit, corrupti fugiat!
+    </div>
   </div>
 </template>
 
@@ -10,6 +15,9 @@
       return {
         title: 'Youtube'
       }
+    },
+    components: {
+      TheSideBar: () => import('@/components/TheSideBar')
     }
   }
 </script>
