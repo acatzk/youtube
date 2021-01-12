b4b345688de165bedfbcd5417f3fff0da7666f08
Joshua Galit <joshuaimalay@gmail.com>
2021-01-12 15:06:06 +0800

    add partial post video youtube post

---
+++ components/PostCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <a href="#">
+    <img class="object-cover h-32 w-full transition ease-in-out duration-150 opacity-100 hover:opacity-75" src="@/assets/images/avatar.jpg" alt="">
+    <div class="text-sm leading-tight pt-1">
+      <a href="#">PIGGY PIGGY DUDE ON NIGHTMARE MODE!!</a>
+    </div>
+    <div class="text-sm mt-1 leading-tight">
+      <a href="#" class="text-gray-600 hover:text-gray-800 flex items-center">
+        <span>DashieGames</span>
+        <span>
+          <svg class="pl-1 w-4 h-4" fill="currentColor" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" preserveAspectRatio="xMidYMid meet"><path xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" d="M6.267 3.455a3.066 3.066 0 001.745-.723 3.066 3.066 0 013.976 0 3.066 3.066 0 001.745.723 3.066 3.066 0 012.812 2.812c.051.643.304 1.254.723 1.745a3.066 3.066 0 010 3.976 3.066 3.066 0 00-.723 1.745 3.066 3.066 0 01-2.812 2.812 3.066 3.066 0 00-1.745.723 3.066 3.066 0 01-3.976 0 3.066 3.066 0 00-1.745-.723 3.066 3.066 0 01-2.812-2.812 3.066 3.066 0 00-.723-1.745 3.066 3.066 0 010-3.976 3.066 3.066 0 00.723-1.745 3.066 3.066 0 012.812-2.812zm7.44 5.252a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg> 
+        </span>
+      </a>
+      <span class="text-sm text-gray-600">618k views &middot; 2 days ago</span>
+    </div>
+  </a>
+</template>
