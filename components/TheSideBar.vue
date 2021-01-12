b8a7ad24f11ae9c8fcd910cce5e9f10f8db62597
Joshua Galit <joshuaimalay@gmail.com>
2021-01-12 09:17:35 +0800

    Partial deesign

---
+++ components/TheSideBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <ul class="mt-3 space-y-1">
+      <li>
+        <a href="#" class="flex items-center space-x-3 hover:bg-gray-300 py-2 transition ease-in-out duration-150">
+          <span class="pl-6">
+            <svg class="w-6 h-6 text-red-600" fill="currentColor" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" preserveAspectRatio="xMidYMid meet"><path xmlns="http://www.w3.org/2000/svg" d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
+          </span>
+          <span class="text-base text-gray-700">Home</span>
+        </a>
+      </li>
+      <li>
+        <a href="#" class="flex items-center space-x-3 hover:bg-gray-300 py-2 transition ease-in-out duration-150">
+          <span class="pl-6">
+            <svg class="w-6 h-6 text-gray-600" fill="currentColor" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" preserveAspectRatio="xMidYMid meet"><path xmlns="http://www.w3.org/2000/svg" d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
+          </span>
+          <span class="text-base text-gray-600">Home</span>
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
