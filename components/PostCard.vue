caf11dd509fccb1e4ba0fcfdb85bf3d37a73e7f0
Joshua Galit <joshuaimalay@gmail.com>
2021-01-14 18:05:57 +0800

    youtube posts and content design - DONE

--- components/PostCard.vue
+++ components/PostCard.vue
@@ -4,11 +4,13 @@
       <div class="flex-shrink-0 relative">
         <img class="w-full bg-gray-400 border-3 border-black overflow-hidden" :src="post.thumbnailUrl" alt="avatar">
         <div class="absolute bg-black text-white px-2 text-xs -mt-5 right-0 font-medium">7:34</div>
-        <div class="absolute top-0 left-0 flex flex-row space-y-3">
-          <div>
-            <button></button>
-          </div>
-          <div></div>
+        <div class="absolute top-0 right-0 flex flex-col mt-1 space-y-1 mr-1" v-show="false">
+          <button class="focus:outline-none">
+            <svg class="w-6 h-6 fill-current  bg-gray-900 text-white" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false"><path d="M12 3.67c-4.58 0-8.33 3.75-8.33 8.33s3.75 8.33 8.33 8.33 8.33-3.75 8.33-8.33S16.58 3.67 12 3.67zm3.5 11.83l-4.33-2.67v-5h1.25v4.34l3.75 2.25-.67 1.08z" class="style-scope yt-icon"></path></svg>
+          </button>
+          <button class="focus:outline-none bg-gray-900 text-white">
+            <svg class="w-6 h-6 fill-current" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false"><path d="M3.67 8.67h14V11h-14V8.67zm0-4.67h14v2.33h-14V4zm0 9.33H13v2.34H3.67v-2.34zm11.66 0v7l5.84-3.5-5.84-3.5z" class="style-scope yt-icon"></path></svg>
+          </button>
         </div>
       </div>
       <div class="text-sm leading-tight flex items-start space-x-2 pt-3">
@@ -30,7 +32,7 @@
           </div>
         </div>
         <button class="flex-shrink-0">
-          <svg class="text-gray-700 h-6 h-6 fill-current" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false"><path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z" class="style-scope yt-icon"></path></svg>
+          <svg class="text-gray-700 h-6 w-6 fill-current" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false"><path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z" class="style-scope yt-icon"></path></svg>
         </button>
       </div>
     </a>
--- pages/index.vue
+++ pages/index.vue
@@ -1,75 +1,29 @@
 <template>
   <div class="flex-1 flex">
     <TheSideBar />
-    <div class="flex-1 overflow-y-auto bg-white">
-      <div class="container mx-auto max-w-7xl py-6 px-6">
+    
+    <div class="flex-1 overflow-y-auto bg-gray-100">
 
-        <div class="font-semibold mb-5 text-gray-700">Recommended</div>
-        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-2 mb-6 border-b">
-          <div v-for="i in 18" :key="i">
-            <PostCard />
-          </div>
-          <div class="py-4">
-            <a href="#" class="uppercase text-sm font-medium text-gray-500 hover:text-gray-600">Show more</a>
-          </div>
-        </div>
-
-        <div class="flex justify-between items-center mb-6">
-          <div class="flex items-center">
-            <a href="#" class="flex-shrink-0">
-              <img class="rounded-full object-cover border w-8 h-8" src="@/assets/images/avatar.jpg" alt="avatar">
-            </a>
-            <a class="ml-2 font-medium" href="#">Angryboy</a>
-          </div>
-          <a href="#">
-            <div class="text-2xl font-light text-gray-600 hover:text-gray-800">&times;</div>
-          </a>
-        </div>
-        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-2 mb-6 border-b pb-6">
-          <div v-for="i in 6" :key="i">
-            <PostCard />
+      <!-- <div class=" w-auto px-12">
+        <VueHorizontalList :items="tags">
+          <template #nav-prev>
+            <div>👈</div>
+          </template>
+          <template #default="{ item }" :options="options">
+            <button class=" py-2 px-3 bg-gray-700 text-gray-100 rounded-full text-xs border truncate font-medium">{{ item }}</button>
+          </template>
+          <template #nav-next>
+            <div>👉</div>
+          </template>
+        </VueHorizontalList> 
+      </div> -->
+      
+      <div class="container mx-auto max-w-screen-xl w-full px-4 py-6">
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-4 gap-y-8 mb-6 "> <!-- border-b -->
+          <div v-for="(post, i) in posts" :key="i">
+            <PostCard :post="post"/>
           </div>
         </div>
-
-
-        <div class="flex justify-between items-center mb-6">
-          <div class="flex items-center">
-            <a href="#" class="flex-shrink-0">
-              <img class="rounded-full object-cover border w-8 h-8" src="@/assets/images/avatar.jpg" alt="avatar">
-            </a>
-            <a class="ml-2 font-medium" href="#">angrybird</a>
-          </div>
-          <a href="#">
-            <div class="text-2xl font-light text-gray-600 hover:text-gray-800">&times;</div>
-          </a>
-        </div>
-        <div class="pb-6">
-          <a href="#" class="flex flex-col sm:flex-row">
-            <div class="flex-shrink-0">
-              <img class="object-cover w-full sm:w-48 h-32 transition ease-in-out duration-150 opacity-100 hover:opacity-75" src="@/assets/images/avatar.jpg" alt="">
-            </div>
-            <div class="flex-1 ml-0 sm:ml-4 flex flex-col">
-              <div class="text-sm leading-tight pt-1">
-                <a href="#">Machine Learning Tutorial for Beginners - USING JAVASCRIPT!</a>
-              </div>
-              <div class="text-sm mt-1 leading-tight max-w-128 w-full">
-                <a href="#" class="text-gray-600 hover:text-gray-800 flex flex-col sm:flex-row items-right sm:items-center">
-                  <div class="flex items-center">
-                    <span>DashieGames</span>
-                    <span>
-                      <svg class="pl-1 w-4 h-4" fill="currentColor" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" preserveAspectRatio="xMidYMid meet"><path xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" d="M6.267 3.455a3.066 3.066 0 001.745-.723 3.066 3.066 0 013.976 0 3.066 3.066 0 001.745.723 3.066 3.066 0 012.812 2.812c.051.643.304 1.254.723 1.745a3.066 3.066 0 010 3.976 3.066 3.066 0 00-.723 1.745 3.066 3.066 0 01-2.812 2.812 3.066 3.066 0 00-1.745.723 3.066 3.066 0 01-3.976 0 3.066 3.066 0 00-1.745-.723 3.066 3.066 0 01-2.812-2.812 3.066 3.066 0 00-.723-1.745 3.066 3.066 0 010-3.976 3.066 3.066 0 00.723-1.745 3.066 3.066 0 012.812-2.812zm7.44 5.252a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg> 
-                    </span>
-                  </div>
-                  <span class="ml-0 mt-3 sm:mt-0 sm:ml-2 text-sm text-gray-600">618k views &middot; 2 days ago</span>
-                </a>
-                <div class="mt-3 text-gray-600 leading-snug tracking-wide">
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam quisquam mollitia laboriosam porro esse, magnam et. Illum, itaque, quos odit sit aperiam ea id expedita aut officiis totam voluptas. Quibusdam.
-                </div>
-              </div>
-            </div>
-          </a>
-        </div>
-
       </div>
     </div>
   </div>
@@ -85,6 +39,140 @@
     components: {
       TheSideBar: () => import('@/components/TheSideBar'),
       PostCard: () => import('@/components/PostCard')
+    },
+    data () {
+      return {
+        posts: [
+          {
+            thumbnailUrl: 'https://i.ytimg.com/vi/gGagPdT3L-s/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAiR7NDTL6cpZ-lMLCz6opGI4YN3Q',
+            avatar: 'https://yt3.ggpht.com/a-/AOh14Gio4MyDJ8edH8qbyQt7Ii4w7fCIVQ0XtOeOvA=s68-c-k-c0x00ffffff-no-rj-mo',
+            title: 'Bob Marley Greatest Hits Reggae Songs 2018 - Bob Marley Full Album',
+            channel: 'Yuu Mii',
+            isVerified: false,
+            views: '6,855,043 views',
+            created_at: '2 years ago'
+          },
+          {
+            thumbnailUrl: 'https://i.ytimg.com/an_webp/JHCKab2oS4s/mqdefault_6s.webp?du=3000&sqp=CLX7_v8F&rs=AOn4CLDBZCIjPDpjPRb3H6CcIsM2VkI_BA',
+            avatar: 'https://yt3.ggpht.com/a-/AOh14Gj286c5gsHOdd8GjPlLu-NdKL79PGJjFhrtwQ=s68-c-k-c0x00ffffff-no-rj-mo',
+            title: 'Final Touches & Deploying the Blog | Nuxt.js & Storyblok - Building a Complete Blog',
+            channel: 'Academind',
+            isVerified: true,
+            views: '15,001 views',
+            created_at: '2 years ago'
+          },
+          {
+            thumbnailUrl: 'https://i.ytimg.com/an_webp/rncY1tlWShM/mqdefault_6s.webp?du=3000&sqp=CKHT_v8F&rs=AOn4CLCJdjoWXuvO7VmpD94lGFlrl-xqzQ',
+            avatar: 'https://yt3.ggpht.com/a-/AOh14Gj2IltSGzbRNN4N18hIhgLY6_cRMx0DVkjxmA=s68-c-k-c0x00ffffff-no-rj-mo',
+            title: 'Intro to Vue 3 + Composition API: Builda Todo App',
+            channel: 'Coding Garden',
+            isVerified: false,
+            views: '48k views',
+            created_at: '3 months ago'
+          },
+          {
+            thumbnailUrl: 'https://i.ytimg.com/an_webp/5RluSnRPRbI/mqdefault_6s.webp?du=3000&sqp=CJrt_v8F&rs=AOn4CLCETAkk1X2t33Cpzep_wMyNqz4lwA',
+            avatar: 'https://yt3.ggpht.com/a-/AOh14GixXc5ib6-B903882xRSj-Z3NoHMkj1OA-J8Q=s68-c-k-c0x00ffffff-no-rj-mo',
+            title: 'UI Design Trends 2021',
+            channel: 'DesignSense',
+            isVerified: false,
+            views: '110k views',
+            created_at: '2 weeks ago'
+          },
+          {
+            thumbnailUrl: 'https://i.ytimg.com/an_webp/UG0-V95qmPg/mqdefault_6s.webp?du=3000&sqp=CJjp_v8F&rs=AOn4CLDxDBfiz115zwQXxzd3vNZUuJItUw',
+            avatar: 'https://yt3.ggpht.com/a-/AOh14GiGo0Ncc6uH_M8VYvPZ690hjnnMZCIwN6WvzQ=s68-c-k-c0x00ffffff-no-rj-mo',
+            title: 'Add Any Pattern to Clothes i Photoshop!',
+            channel: 'PiXimperfect',
+            isVerified: true,
+            views: '110k views',
+            created_at: '2 weeks ago'
+          },
+          {
+            thumbnailUrl: 'https://i.ytimg.com/an_webp/-_X6PhkjpzU/mqdefault_6s.webp?du=3000&sqp=CNnH_v8F&rs=AOn4CLCi5nnRE5QKMCixlDVeVB6MEDd0zw',
+            avatar: 'https://yt3.ggpht.com/a-/AOh14GgRakJo9HGmRzwTAjdcYFMYBLCl1fP2Z4YgbA=s68-c-k-c0x00ffffff-no-rj-mo',
+            title: '5 Front-end Development Skills to Land Your First Job',
+            channel: 'Programming with Mosh',
+            isVerified: true,
+            views: '65k views',
+            created_at: '6 days ago'
+          },
+          {
+            thumbnailUrl: 'https://i.ytimg.com/an_webp/aoQ6S1a32j8/mqdefault_6s.webp?du=3000&sqp=CPDp_v8F&rs=AOn4CLA81GUrPEAOH1YTwo2dID7Wwaxvdg',
+            avatar: 'https://yt3.ggpht.com/a-/AOh14GgwaiCp63JOClbTXswJ4u8x9IXpD_qDn3tt3g=s68-c-k-c0x00ffffff-no-rj-mo',
+            title: 'How to Make a Landing Page using HTML, SCSS, and Javascript - FULL COURSE',
+            channel: 'freeCodeCamp.org',
+            isVerified: true,
+            views: '27K views',
+            created_at: '14 hours ago'
+          },
+          {
+            thumbnailUrl: 'https://i.ytimg.com/an_webp/j-3RwvWZoaU/mqdefault_6s.webp?du=3000&sqp=COz8_v8F&rs=AOn4CLAVV7oPDjrNfGd5gs5pOmOElVmz2g',
+            avatar: 'https://yt3.ggpht.com/a-/AOh14GiclTRAzNjdjvYZFkQzK81ROHt7JxnPuK_wyA=s68-c-k-c0x00ffffff-no-rj-mo',
+            title: 'Why You Should Learn Server Middleware with Nuxt.js! A step by step guide',
+            channel: 'Program with Erik',
+            isVerified: false,
+            views: '34K views',
+            created_at: '2 years ago'
+          },
+          {
+            thumbnailUrl: 'https://i.ytimg.com/an_webp/hGg8L7Xlrgc/mqdefault_6s.webp?du=3000&sqp=COH9_v8F&rs=AOn4CLBDCKO3I8H-hSdkJzN-j9kGzTl8yg',
+            avatar: 'https://yt3.ggpht.com/a-/AOh14GhX1kTmCNgahq2-PG6cV6fW9V75FAi1ECaoDg=s68-c-k-c0x00ffffff-no-rj-mo',
+            title: '10 annoying things about working at Google that irritated me',
+            channel: 'TechLead',
+            isVerified: true,
+            views: '242K views',
+            created_at: '2 years ago'
+          },
+          {
+            thumbnailUrl: 'https://i.ytimg.com/an_webp/B7J8Idx3hyY/mqdefault_6s.webp?du=3000&sqp=CKiB__8F&rs=AOn4CLAFFMTzYc4zXLC2pttuaKDF5Arhvw',
+            avatar: 'https://yt3.ggpht.com/a-/AOh14GgHDvPH4HVnXEX892SE2H5gtdyjosxeaEeOHw=s68-c-k-c0x00ffffff-no-rj-mo',
+            title: 'Biggest MISCONCEPTIONS about Becoming a Self-Taugh Programmer',
+            channel: 'Andy Sterkowitz',
+            isVerified: false,
+            views: '24K views',
+            created_at: '3 months ago'
+          },
+          {
+            thumbnailUrl: 'https://i.ytimg.com/an_webp/sslKTYxMl7c/mqdefault_6s.webp?du=3000&sqp=CNj0_v8F&rs=AOn4CLCqSINgoO59Ov7dg0sb6rRQkLxi8g',
+            avatar: 'https://yt3.ggpht.com/a-/AOh14Gh91SDLonPENSz1NifM4j62B3YkPGZ9DI4mXw=s68-c-k-c0x00ffffff-no-rj-mo',
+            title: 'FlipTop - Most "SAVAGE" moments P1',
+            channel: 'Hiphop Heads TV',
+            isVerified: false,
+            views: '261K views',
+            created_at: '2 weeks ago'
+          },
+          {
+            thumbnailUrl: 'https://i.ytimg.com/an_webp/SNRFGw5Mb0g/mqdefault_6s.webp?du=3000&sqp=CMCM__8F&rs=AOn4CLDvJ2MIsJzHivILavH7wNxAI04tRw',
+            avatar: 'https://yt3.ggpht.com/a-/AOh14GgWJLbSHT1NxPtwloxTowosulZRmALCIr1Abg=s68-c-k-c0x00ffffff-no-rj-mo',
+            title: 'How To Build JavaScript Stuck',
+            channel: 'Web Dev Simplified',
+            isVerified: true,
+            views: '17K views',
+            created_at: '1 month ago'
+          } 
+        ],
+        tags: [
+          'All',
+          'Air Supply',
+          'Love Songs',
+          'Lo-fi music',
+          'NBA',
+          'JavaScript',
+          'Linux',
+          'GraphQL',
+          'Computer Science',
+          'TV Patrol',
+          'Anime',
+          'Adobe Premiere Pro',
+          'New-Age Music',
+          'Adobe After Effects',
+          'Computer programming',
+          'Recently uploaded',
+          'Live',
+          'Mixes'
+        ]
+      }
     }
   }
 </script>
