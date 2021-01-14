8742cd2a9a5d03fc99aa127af16f21764c0c854e
Joshua Galit <joshuaimalay@gmail.com>
2021-01-14 10:27:33 +0800

    register vue-horizontal-list in plugins array

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,6 +21,7 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
+    '~/plugins/vue-horizontal-list.js'
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
