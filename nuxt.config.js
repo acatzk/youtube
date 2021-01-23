c1b3937a05d2d4b6e90adc2aa1962f57ebe4d4ad
Joshua Galit <joshuaimalay@gmail.com>
2021-01-23 23:31:17 +0800

    register vue2-perfect-scrollbar plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,8 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-    '~/plugins/vue-horizontal-list.js'
+    '~/plugins/vue-horizontal-list.js',
+    '~/plugins/vue2-perfect-scrollbar.js'
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
